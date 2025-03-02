140597df5c0c1f4381b916118ac1a7076c575e9b
Igor Coelho <xh4rdzn@gmail.com>
2025-03-02 12:04:09 -0300

    add: screenshots

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,7 @@
 import cookie from '@fastify/cookie'
 import fastify from 'fastify'
 import { env } from './env'
+import { mealsRoutes } from './routes/mealsRoutes'
 import { usersRoutes } from './routes/usersRoutes'
 
 const app = fastify()
@@ -8,6 +9,10 @@ const app = fastify()
 app.register(cookie)
 app.register(usersRoutes, {
   prefix: '/users',
+})
+
+app.register(mealsRoutes, {
+  prefix: '/meals',
 })
 
 app
